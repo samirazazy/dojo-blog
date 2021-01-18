6b2d67f2299c632536d3ba15c004343309c3ea39
samirazazy <samiralazazy@gmail.com>
2021-01-18 22:57:14 +0200

    #18 - Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   // Crete  fack server
   // npx json-server --watch data/db.json --port 8000
   const [Blogs, setBlogs] = useState(null);
+  const [Loading, setLoading] = useState(true);
 
   const handelDelete = (id) => {
     const newBolgs = Blogs.filter((blog) => blog.id !== id);
@@ -19,14 +20,19 @@ const Home = () => {
   }, []);
 
   const getData = async () => {
-    const res = await fetch('http://localhost:8000/blogs');
-    const data = await res.json();
-    setBlogs(data);
+    setTimeout(async () => {
+      const res = await fetch('http://localhost:8000/blogs');
+      const data = await res.json();
+      setBlogs(data);
+      setLoading(false);
+    }, 1000);
   };
 
   return (
     <div className='home'>
-      {Blogs && (
+      {Loading ? (
+        <p>Loading...</p>
+      ) : (
         <BlogList
           Blogs={Blogs}
           title={'Blog List'}
