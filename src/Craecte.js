ab61f6e1b38870f2d34ed246a8fdc697834a6a70
samirazazy <samiralazazy@gmail.com>
2021-01-19 23:44:12 +0200

    #27 - Controlled Inputs (forms)

--- src/Craecte.js
+++ src/Craecte.js
@@ -1,7 +1,38 @@
+import { useState } from 'react';
+
 const Craecte = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [auther, setAuther] = useState('azazy');
+
   return (
-    <div className='craete'>
-      <h2>Add new Blog</h2>
+    <div className='create'>
+      <form>
+        <label>Blog Name:</label>
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label>Blog Body:</label>
+        <textarea
+          type='text'
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+
+        <label>Blog Auther:</label>
+        <select value={auther} onChange={(e) => setAuther(e.target.value)}>
+          <option value='samir'>samir</option>
+          <option value='samir'>azazy</option>
+        </select>
+      </form>
+      <div>{title}</div>
+      <div>{body}</div>
+      <div> {auther}</div>
     </div>
   );
 };
